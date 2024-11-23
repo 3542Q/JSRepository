354aa0b18aa6a4f2245f777eed667a6d0ff6e5c2
3542Q <3542953093@qq.com>
2024-11-24 02:32:25 +0800

    update

--- articles/articles.js
+++
@@ -1,9 +0,0 @@
-const articles = [
-  {
-    articleID: 1,
-    title: '文章1',
-    titleImageURL: 'https://5b1b1bc5.telegraph-image-6fo.pages.dev/file/97c2b6ec6477aef3e7392-84671b3a8aa34035f1.png',
-    contentID: 1,
-    contentURL: 'https://raw.githubusercontent.com/3542Q/JSRepository/main/contents/contents_1.js',
-  },
-]
